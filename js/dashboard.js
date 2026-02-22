3acdbd4310d8defb0eef5862caab73e66e3e1199
Usman Anwar <usmananwar@outlook.com>
2026-02-22 16:14:47 -0500

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -349,7 +349,7 @@ async function exportFile() {
         if (res.ok) {
             // Get the filename from Content-Disposition header if available
             const contentDisposition = res.headers.get('Content-Disposition');
-            let filename = 'export.csv'; // default filename
+            let filename = 'export.xslx'; // default filename
             
             if (contentDisposition) {
                 const filenameMatch = contentDisposition.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/);
